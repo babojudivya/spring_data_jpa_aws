7c0c3e186f85ecab332667d9c5fae3b1c10cfbd5
kishoremarojuu <kishoremarojuu@gmail.com>
2020-08-28 18:59:24 -0500

    delete compilation issue class

--- src/test/java/com/techprimers/spring_datajpa_rds/SpringDatajpaRdsApplicationTests.java
+++ src/test/java/com/techprimers/spring_datajpa_rds/SpringDatajpaRdsApplicationTests.java
@@ -5,15 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringDatajpaRdsApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
 public class SpringDatajpaRdsApplicationTests {
 
 	@Test
